597b3f5b8482c3b3424bdd05d2c05f6e6cf37b73
Ross Cairns <ross@electricglen.com>
2016-05-18 18:49:05 +0100

    Added webserver

--- script-run-bene-installation.sh
+++ script-run-bene-installation.sh
@@ -1,15 +1,104 @@
 #!/usr/bin/env node
 
+// Modules
+// =======
+
 var child_process = require( 'child_process' );
+var os = require( 'os' );
 
-console.log("Hello World");
+// Config
+// ======
+var IS_VIDEO_MASK = 128;
 
-child_process.exec( './libpidee/examples/bin/print-dip', onPideeValue );
 
-function onPideeValue ( error, stdout, stderror ) {
+// Startup
+// =======
 
-	var pideeDipValue = parseInt( stdout, 10 );
+getPideeValue( function ( value ) {
+    
+    if ( getMaskIsSet( value, IS_VIDEO_MASK ) ) {
 
-	console.log( 'value is', pideeDipValue );
+        startVideoPlayer( value - IS_VIDEO_MASK );
+        
+    } else {
 
+        startWebApp( number );
+        
+    }
+    
+});
+
+// Runners
+// =======
+
+// Startup Video
+// -------------
+
+function startVideoPlayer ( number ) {
+    
 }
+
+// Startup Webapp
+// --------------
+
+function startWebApp ( number ) {
+
+    // Start the webserver serving
+    var serverProcess = child_process.spawn( 'python', [ '-m', 'SimpleHTTPServer', '3000' ] );
+
+    // Start the webbrowser
+    var webBrowserProcess = child_process.spawn( 'epiphany-browser', [ 'http://localhost:3000/html/' + number + '.html' ] );
+
+    // Press the fullscreen key
+    setTimeout( function () {
+         child_process.spawn( 'xte', [ '"key F11"', '-x:0' ] );
+    }, 25000 );
+    
+    
+}
+
+// Utils
+// =====
+
+// Get Pidee Value
+// ---------------
+
+function getPideeValue ( callback ) {
+
+    var platform = os.platform();
+
+    if ( platform == 'darwin' ) {
+        var value = 17;
+        console.log( 'On OSX, faking value with', value );
+        setImmediate( function () {
+            callback( value );
+        });
+        return;
+    }
+
+    child_process.exec( './libpidee/examples/bin/print-dip', onPideeValue );
+
+    function onPideeValue ( error, stdout, stderror ) {
+            var value = parseInt( stdout, 10 );
+            console.log( 'Pidee value is', value );
+            callback( value );
+    }
+    
+}
+
+function getMaskIsSet ( value, mask ) {
+    return (value & mask) !== 0;
+}
+
+
+// console.log("Hello World");
+
+// child_process.exec( './libpidee/examples/bin/print-dip', onPideeValue );
+
+// function onPideeValue ( error, stdout, stderror ) {
+
+// 	var pideeDipValue = parseInt( stdout, 10 );
+
+// 	console.log( 'value is', pideeDipValue );
+
+// }
